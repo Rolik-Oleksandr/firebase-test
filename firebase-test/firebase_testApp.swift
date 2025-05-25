f461fe74a6147c457772f3be4861f9210223bbd1
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-05-26 01:24:38 +0300

    Initial Commit

---
+++ firebase-test/firebase_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_testApp.swift
+//  firebase-test
+//
+//  Created by Oleksandr Rolik on 26.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct firebase_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
