9f39a2ffd3377b352beef89815c8fa512147418b
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-05-27 21:51:40 +0300

    configured firebase

--- firebase-test/firebase_testApp.swift
+++ firebase-test/firebase_testApp.swift
@@ -1,17 +1,24 @@
-//
-//  firebase_testApp.swift
-//  firebase-test
-//
-//  Created by Oleksandr Rolik on 26.05.2025.
-//
-
 import SwiftUI
+import Firebase
 
 @main
 struct firebase_testApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("Configured Firebase")
+        
+        return true
     }
 }
