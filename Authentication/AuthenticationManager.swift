721a26666a91a3a64e66e39da79ef510ae810b6f
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-06-04 23:40:23 +0300

    signIn and resset password functions

--- Authentication/AuthenticationManager.swift
+++ Authentication/AuthenticationManager.swift
@@ -26,9 +26,20 @@ final class AuthenticationManager {
         return AuthDataResultModel(user: user)
     }
     
+    @discardableResult
     func createUser(email: String, password: String) async throws -> AuthDataResultModel {
         let AuthDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
         return AuthDataResultModel(user: AuthDataResult.user)
+    }
+    
+    @discardableResult
+    func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+    func resetPassword(email: String) async throws {
+        try await Auth.auth().sendPasswordReset(withEmail: email)
     }
     
     func signOut() throws {
